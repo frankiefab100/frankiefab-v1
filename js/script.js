ff53d1c9779006168ba889c1d0c62274fde18e2f
frankiefab100 <ohaegbulamfrank@gmail.com>
2022-02-21 16:57:15 +0100

    update files and style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// script
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".nav");
+const scrollup = document.querySelector(".scroll-btn");
+
+hamburgerMenu.addEventListener("click", () => {
+  const navLink = document.querySelector(".nav-links");
+  navLink.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= 100) {
+    scrollup.classList.add("active");
+  } else {
+    scrollup.classList.remove("active");
+  }
+});
