83e51bb3c4ed587baa2ca9da63440e4ba4251377
frankiefab100 <ohaegbulamfrank@gmail.com>
2022-02-22 16:42:57 +0100

    update ui

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".nav");
+const navBar = document.querySelector("nav");
+const navMenu = document.querySelector(".nav-links");
+const navItem = document.querySelector(".nav-links li");
 const scrollup = document.querySelector(".scroll-btn");
 
+// Show Hamburger Menu
 hamburgerMenu.addEventListener("click", () => {
-  const navLink = document.querySelector(".nav-links");
-  navLink.classList.toggle("active");
+  if (!navMenu.classList.contains("active")) {
+    navMenu.classList.add("active");
+  } else {
+    closeMenu();
+  }
 });
+
+// Close Hamburger Menu
+function closeMenu() {
+  navMenu.classList.remove("active");
+}
+
+navItem.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= 100) {
