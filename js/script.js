78b0d06501c83ca6275174aa185c506281e74941
frankiefab100 <ohaegbulamfrank@gmail.com>
2022-02-23 02:02:04 +0100

    Update ui

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const scrollup = document.querySelector(".scroll-btn");
 hamburgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   hamburgerMenu.classList.toggle("active");
+  document.body.classList.add("stop-scroll");
 });
 
 // Close Hamburger Menu
@@ -16,12 +17,52 @@ navItem.forEach((link) => link.addEventListener("click", closeMenu));
 function closeMenu() {
   navMenu.classList.remove("active");
   hamburgerMenu.classList.remove("active");
+  document.body.classList.remove("stop-scroll");
 }
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset >= 100) {
+  if (window.pageYOffset >= 50) {
     scrollup.classList.add("active");
   } else {
     scrollup.classList.remove("active");
   }
 });
+
+// Dark Mode Theme Switch
+const themeSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function changeTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme-color", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme-color", "light");
+  }
+}
+
+themeSwitch.addEventListener("change", changeTheme, false);
+
+//  Save selected Mode for future
+function changeTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme-color", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme-color", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+// Save Mode on load
+const selectedTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (selectedTheme) {
+  document.documentElement.setAttribute("data-theme-color", selectedTheme);
+
+  if (selectedTheme === "dark") {
+    themeSwitch.checked = true;
+  }
+}
